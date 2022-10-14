d035d2b4819df99bd4192f385f16a743e4f56c9c
Andres0051 <charles_051@hotmail.com>
2022-10-13 22:38:54 -0500

    layout1/styles/home

--- views/components/header.php
+++ views/components/header.php
@@ -8,12 +8,12 @@ if (empty($_SESSION['sessionImage'])) {
 }
 
 ?>
-<header>
+<header> 
     <div class="group-header">
         <div class="contenedor-Logo">
             <div class="contorno">
                 <div class="logo">
-                    <img src="<?PHP constant('URL') ?>/public/img/utilities/logo-apprender.png" alt="">
+                    <img src="<?PHP constant('URL') ?>/public/img/utilities/logo.png" alt="">
                 </div>
                 <div class="nombre-proyecto">
                     <label>
